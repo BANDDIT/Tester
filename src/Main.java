349f7d78d601b124a1de5925e5c6fcd3665bdaea
BANDDIT <arrickrussellg@gmail.com>
2023-01-09 15:40:39 +0700

    ini ketiga

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,7 @@ public class Main {
 		System.out.println("Hello world");
 		System.out.println("Bye");
 		System.out.println("Jangan lupa datang kembali"); 
+		System.out.println("Ini tambahan bwt ketiga");
 	}
 
 }
